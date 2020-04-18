697029734f3fb787019de24135226a709156f47e
ozkanyldrm55 <58321853+ozkanyldrm55@users.noreply.github.com>
2020-04-18 17:14:43 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,26 +1,24 @@
+/* bu uygulamada ledler i = 0 dan 100 e kadar yavas yavas parlakligi artacak ve sonrasinda tam tersi sekilde parlakligi azalacak. */
+
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
 
 uint32_t delay_counter;
 
-GPIO_InitTypeDef 		GPIO_InitStruct;
+GPIO_InitTypeDef GPIO_InitStruct;
 TIM_TimeBaseInitTypeDef TIM_InitStruct;
-TIM_OCInitTypeDef 		TIMOC_InitStruct; // PWM icin
+TIM_OCInitTypeDef TIMOC_InitStruct; // PWM icin
 
-void SysTick_Handler() //1ms de bir bu fonk gelecek
-{
+void SysTick_Handler() { // 1ms de bir bu fonk gelecek
 	if(delay_counter > 0)
 		delay_counter--;
 }
 
-void delay_ms(uint32_t time) //time degerine mesela 10 atadýk sistem her 1ms de SysTick_Handler fonk gidecek
-{
+void delay_ms(uint32_t time) { // time degerine mesela 10 atadik sistem her 1ms de SysTick_Handler fonk gidecek
 	delay_counter = time;
-	while(delay_counter); // delay_counter 0 olana kadar bekle demek
+	while(delay_counter);  // delay_counter 0 olana kadar bekle demek
 }
-
-void GPIO_Config()
-{
+void GPIO_Config() {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
 
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;  // timer olarak kullanildigi icin AF olarak kullandik.
@@ -37,16 +35,22 @@ void GPIO_Config()
 	GPIO_PinAFConfig(GPIOD,GPIO_PinSource15,GPIO_AF_TIM4);
 }
 
+void TIM_Config() {
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4 , ENABLE);
+	
+ 	  /* PWM FREKANSI OLUSTURMAK ICIN ASAGIDAKI FORMULLER KULLANILIR
+	   * -> Periyod = (Timer_Tick_Freq / PWM_Freq)-1
+	   * -> PWM_Freq = Timer_Tick_Freq / (periyod + 1)
+	   * -> Time_Tick_Freq = Timer_CLK /(Prescaler + 1) 
+	   *
+	   *  //pulse oranini hesaplarken
+	   *  pulse = (periyod + 1) * (duty cycl %) - 1
 
-void TIM_Config()
-{
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4,ENABLE);
-
-	/* PWM_FREQ = 10KHz olmasýný istedik 10000mhz
+	/* PWM_FREQ = 10KHz( 10 000 mhz) olmasini istedik
 	 *
-	 * tim_tic = 84000000 / (psc+1) = 1000000 , psc = 83
+	 * Time_Tick_Freq = 84000000 / (Prescaler + 1) = 1000000 , Prescaler = 83 olarak bulunur
 	 *
-	 * periyod = (1000000 / 1000=PWM_FREQ)-1 = 99
+	 * Periyod = (1000000 / 10 000) - 1 = 99
 	 **/
 
 	TIM_InitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
@@ -58,22 +62,22 @@ void TIM_Config()
 	TIM_TimeBaseInit(TIM4,&TIM_InitStruct);
 	TIM_Cmd(TIM4,ENABLE);
 
-	TIMOC_InitStruct.TIM_OCMode = TIM_OCMode_PWM1;//PWM mod1 mod2 muhabbeti , doluluk oranlarýyla ilgili
-	TIMOC_InitStruct.TIM_OutputState = ENABLE; //cikisi aktif ettik
+	TIMOC_InitStruct.TIM_OCMode = TIM_OCMode_PWM1;	// PWM mod1 da duty cycle 25 ise %25 doluluk alinir
+							// PWM mod2 dE duty cycle 25 ise %75 doluluk alinir.aralarindaki fark budur.
+	TIMOC_InitStruct.TIM_OutputState = ENABLE; 	// cikisi aktif ettik
 	TIMOC_InitStruct.TIM_OCNPolarity = TIM_OCNPolarity_High;
 
-	SysTick_Config(SystemCoreClock/1000); //1ms de bir kesmeye girer.//LEDLERIN yanma hizini ayarlamak istiyorsak burandan ayarlama yapmaliyiz
-
+	SysTick_Config(SystemCoreClock/1000); // 1ms de bir kesmeye girer.
+					      // LEDLERIN yanma hizini ayarlamak istiyorsak burandan ayarlama yapmaliyiz
+	/* delay fonk yazmak yerine SysTick_Config in kendine has delay fonk kullandik */
 }
 
-int main(void)
-{
+int main(void) {
 	GPIO_Config();
 	TIM_Config();
   while (1)
   {
-	  for(int i = 0; i<=100; i++)
-	  {
+	  for(int i = 0; i <= 100; i++) {
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC1Init(TIM4,&TIMOC_InitStruct);
 		  TIM_OC1PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH1 ENABLE
@@ -81,42 +85,40 @@ int main(void)
 
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC2Init(TIM4,&TIMOC_InitStruct);
-		  TIM_OC2PreloadConfig(TIM4,TIM_OCPreload_Enable);
+		  TIM_OC2PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH2 ENABLE
 		  delay_ms(20);
 
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC3Init(TIM4,&TIMOC_InitStruct);
-		  TIM_OC3PreloadConfig(TIM4,TIM_OCPreload_Enable);
+		  TIM_OC3PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH3 ENABLE
 		  delay_ms(20);
 
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC4Init(TIM4,&TIMOC_InitStruct);
-		  TIM_OC4PreloadConfig(TIM4,TIM_OCPreload_Enable);
+		  TIM_OC4PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH4 ENABLE
 		  delay_ms(20);
 	  }
-	  for(int i = 100; i>=0; i--)
-	  {
+	  for(int i = 100; i >= 0; i--) {
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC1Init(TIM4,&TIMOC_InitStruct);
-		  TIM_OC1PreloadConfig(TIM4,TIM_OCPreload_Enable);
+		  TIM_OC1PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH1 ENABLE
 		  delay_ms(20);
 
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC2Init(TIM4,&TIMOC_InitStruct);
-		  TIM_OC2PreloadConfig(TIM4,TIM_OCPreload_Enable);
+		  TIM_OC2PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH2 ENABLE
 		  delay_ms(20);
 
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC3Init(TIM4,&TIMOC_InitStruct);
-		  TIM_OC3PreloadConfig(TIM4,TIM_OCPreload_Enable);
+		  TIM_OC3PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH3 ENABLE
 		  delay_ms(20);
 
 		  TIMOC_InitStruct.TIM_Pulse = i;
 		  TIM_OC4Init(TIM4,&TIMOC_InitStruct);
-		  TIM_OC4PreloadConfig(TIM4,TIM_OCPreload_Enable);
+		  TIM_OC4PreloadConfig(TIM4,TIM_OCPreload_Enable);//TIM4 -> CH4 ENABLE
 		  delay_ms(20);
 	  }
-
   }
 }
 
@@ -127,14 +129,12 @@ int main(void)
  *
  * PWM_Freq = Timer_Tick_Freq / (periyod + 1)
  *
- *  //pulse oranýný hesaplarken
+ *  //pulse oranÃ½nÃ½ hesaplarken
  *
  *  pulse = (periyod + 1) * (duty cycl %) - 1
  *
  *        = (9999 + 1) * 100/100 -1 = 9999
  */
-
-
 
 
 void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
